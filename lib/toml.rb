2c55ceaa029486557946aa04c351d28248fe1bbb
Chris Schneider <cschneider@comverge.com>
2015-04-30 08:47:33 -0600

    Got it! Quoted values were never even getting checked due to bare values being valid with repeat(0)

--- lib/toml.rb
+++ lib/toml.rb
@@ -18,23 +18,21 @@ module Toml
     rule(:key)    { (match('[a-z]').repeat).as(:key)   }
     rule(:equals) { whitespace >> str("=") >> whitespace }
 
-    rule(:bare_value)   { (match('[a-z]').repeat).as(:value) }
-    rule(:quoted_value) { (str('"') >> match('[^"]').repeat >> str('"')).as(:value) }
+    rule(:bare_value)   { (match('[a-z]').repeat(1)).as(:value) }
+    rule(:quoted_value) { (str('"') >> (match('[^"]').repeat).as(:value) >> str('"')) }
     rule(:value) { (bare_value | quoted_value) >> match('\s').repeat }
 
     rule(:newline) { match('\n') }
     rule(:comment) { (str("#") >> match('[^\n]').repeat).as(:comment) }
-    rule(:whitespace) { match('\s').repeat }
+    rule(:whitespace) { str(" ").repeat }
 
     rule(:key_val) { key >> equals >> value }
 
     def parse
       parser = ((key_val >> newline.repeat) |
-                (comment >> newline.repeat)   ).repeat >>
-               newline.repeat
-      parser = key >> equals >> (bare_value | quoted_value) >> (whitespace)
+                (comment >> newline.repeat)
+               ).repeat >> newline.repeat
       parse_result = parser.parse(raw_string)
-      puts parse_result
 
       parse_result.reduce({}) do |memo, result|
         next memo if result[:comment]
