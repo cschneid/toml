4bb2b6bb202eab5ec4410d480b32b862f1c19359
Chris Schneider <cschneider@comverge.com>
2015-04-29 20:16:09 -0600

    Start sketching what I may want the API would look like

--- lib/toml.rb
+++ lib/toml.rb
@@ -1,5 +1,24 @@
-require "toml/version"
+require 'toml/version'
+require 'parslet'
 
 module Toml
-  # Your code goes here...
+  class Parser
+    include Parslet
+
+    def self.from_file(file)
+      File.open(file) { |io| self.parse(io) }
+    end
+
+    def self.parse(io)
+      instance = self.new(io)
+      instance.parse
+    end
+
+    def initialize(io)
+      @io = i
+    end
+
+    def parse
+    end
+  end
 end
